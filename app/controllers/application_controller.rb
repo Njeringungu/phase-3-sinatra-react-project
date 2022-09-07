0fb0f74cebcb299a7020430cd9083c4e59fe33dc
Njeri Ngungu <janetnjerin89@gmail.com>
2022-09-08 00:18:37 +0300

    Create tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/doctors" do
+    Doctor.all.to_json(include: :patients)
   end
+
+  get "/patients" do
+    Patient.all.to_json
+  end
+
+  get "/appointments" do
+    Appointment.all.to_json
+  end
+
 
 end
